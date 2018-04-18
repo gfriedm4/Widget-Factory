8512b12f3022b440708f1a839806e6dd6149a0c4
Greg Friedman <gfriedm4@johnshopkins.edu>
2018-04-18 13:30:00 -0400

    Text change on homepage

--- resources/assets/js/routes/Home.js
+++ resources/assets/js/routes/Home.js
@@ -7,7 +7,7 @@ export default class Home extends Component {
 				<h1 className="display-4">Hello Widget Wizards!</h1>
 				<p className="lead">Welcome to the Widget Factory Store, where you can satisfy all of your widget buying needs!</p>
 				<hr className="my-4"/>
-				<p>Please enjoy, but don't overstay your welcome!</p>
+				<p>Please enjoy, but don't spend all your money in one place!</p>
 			</div>
 		);
 	}
